eaf910428c84c408f2543c4ac456e20c88348f4e
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2018-05-20 20:26:14 +0300

    Added react and babel loader

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const app = document.querySelector('#app');
+
+function App (props) {
+    return (
+        <div></div>
+    )
+}
+
+ReactDOM.render(<App/>, app);
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,14 @@ module.exports = {
         path: __dirname + '/public',
         filename: 'bundle.js'
     },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
     mode: 'development'
 };
