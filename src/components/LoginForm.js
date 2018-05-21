f1ca9a5065a4e047cb57c50cf2c475d5be84a39e
mikhailsinyakov <mikhail.sinyakov86@gmail.com>
2018-05-21 16:54:43 +0300

    Removed possibility to send the signup form if password less than 6 characters and if password and confirmed password are different

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,64 @@
 import React from 'react';
 
-export default function LoginForm (props) {
-    const action = `/${props.form}`;
-    const confirmPasswordInput = (
-        <span>
-            <input type="password" name="confirmPassword" 
-                    placeholder="Повторите пароль" required />
-            <br/>
-        </span>
-    );
-    const btnName = props.form == 'signup' ? 'Зарегистрироваться'
-                                            : 'Войти';
+export default class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            passwordInput: '',
+            confirmPasswordInput: ''
+        };
+        this.handlePasswordInput = this.handlePasswordInput.bind(this);
+        this.handleConfirmPasswordInput = this.handleConfirmPasswordInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    return (
-        <form action={action} method="POST">
-            <input type="text" name="username" placeholder="Введите имя, ник" required /><br/>
-            <input type="password" name="password" placeholder="Введите пароль" required /><br/>
-            {props.form == 'signup' && confirmPasswordInput}
-            <button type="submit">{btnName}</button>
-        </form>
-    )
+    handlePasswordInput(value) {
+        this.setState({passwordInput: value});
+    }
+
+    handleConfirmPasswordInput(value) {
+        this.setState({confirmPasswordInput: value});
+    }
+
+    handleSubmit(e) {
+        if (this.props.form == 'signup') {
+            if (this.state.passwordInput != this.state.confirmPasswordInput ||
+                this.state.passwordInput.length < 6) {
+                e.preventDefault();
+            }
+        }
+    }
+
+    render() {
+        const action = `/${this.props.form}`;
+        const confirmPasswordInput = (
+            <span>
+                <input type="password" name="confirmPassword" 
+                        onChange={e => this.handleConfirmPasswordInput(e.target.value)}
+                        value={this.state.confirmPasswordInput}
+                        placeholder="Повторите пароль" required />
+                <br/>
+            </span>
+        );
+        const btnName = this.props.form == 'signup' ? 'Зарегистрироваться'
+                                                    : 'Войти';
+
+        return (
+            <form action={action} method="POST">
+                <input type="text" name="username" 
+                        placeholder="Введите имя, ник" required />
+                <br/>
+                <input type="password" name="password" 
+                        onChange={e => this.handlePasswordInput(e.target.value)}
+                        value={this.state.passwordInput}
+                        placeholder="Введите пароль" required />
+                <br/>
+                {this.props.form == 'signup' && confirmPasswordInput}
+                <button type="submit" onClick={e => this.handleSubmit(e)}>
+                    {btnName}
+                </button>
+            </form>
+        );
+    }
+    
 }
